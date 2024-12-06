9392e107ca64dd59045b608989de64d24b6b7c4f
Tiantian Li <tiantiancscs@gmail.com>
2024-12-06 11:39:18 +1100

    Initial commit

---
+++ src/test/java/me/tiantian_li/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.tiantian_li.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
