5ce105cfc5da93de3a999e526aa621787d42258e
Tiantian Li <tiantiancscs@gmail.com>
2024-12-18 12:27:11 +1100

    Create CartDTO.java

---
+++ src/main/java/me/tiantian_li/ecommerce/dto/CartDTO.java
@@ -0,0 +1,12 @@
+package me.tiantian_li.ecommerce.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class CartDTO {
+    private Long id;
+    private Long userId;
+    private List<CartItemDTO> items;
+}
