2b8f1c4454d07a0a2ae821472ce7261790d7818a
Tiantian Li <tiantiancscs@gmail.com>
2024-12-19 11:45:32 +1100

    Update OrderDTO.java

--- src/main/java/me/tiantian_li/ecommerce/dto/OrderDTO.java
+++ src/main/java/me/tiantian_li/ecommerce/dto/OrderDTO.java
@@ -1,6 +1,7 @@
 package me.tiantian_li.ecommerce.dto;
 
 import lombok.Data;
+import me.tiantian_li.ecommerce.model.Order;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -8,10 +9,10 @@ import java.util.List;
 @Data
 public class OrderDTO {
     private Long id;
-    private Long userId;
     private String address;
     private String phoneNumber;
     private Order.OrderStatus status;
     private LocalDateTime createdAt;
+    private Long userId;
     private List<OrderItemDTO> orderItems;
 }
