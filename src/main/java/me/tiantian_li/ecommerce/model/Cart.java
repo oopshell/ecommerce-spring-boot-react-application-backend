809f5cd38d721f49d950d4099e9134e38fbef58a
Tiantian Li <tiantiancscs@gmail.com>
2024-12-19 11:46:08 +1100

    Update Cart.java

--- src/main/java/me/tiantian_li/ecommerce/model/Cart.java
+++ src/main/java/me/tiantian_li/ecommerce/model/Cart.java
@@ -1,4 +1,26 @@
 package me.tiantian_li.ecommerce.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL)
+    private List<CartItem> cartItems = new ArrayList<>();
 }
