5ecaf97955d45c831c9c3bdc76ba6cd46f7d639a
Tiantian Li <tiantiancscs@gmail.com>
2024-12-19 11:46:24 +1100

    Update Order.java

--- src/main/java/me/tiantian_li/ecommerce/model/Order.java
+++ src/main/java/me/tiantian_li/ecommerce/model/Order.java
@@ -1,4 +1,35 @@
 package me.tiantian_li.ecommerce.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "orders")
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String address;
+    private String phoneNumber;
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public enum OrderStatus {
+        PREPARING, DELIVERING, DELIVERED, CANCELLED
+    }
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems = new ArrayList<>();
 }
