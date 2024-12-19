597234836aa9fa4f1a6b4ebab494dddbd6648f60
Tiantian Li <tiantiancscs@gmail.com>
2024-12-19 11:46:19 +1100

    Update CartItem.java

--- src/main/java/me/tiantian_li/ecommerce/model/CartItem.java
+++ src/main/java/me/tiantian_li/ecommerce/model/CartItem.java
@@ -1,4 +1,25 @@
 package me.tiantian_li.ecommerce.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer quantity;
+
+    @ManyToOne
+    @JoinColumn(name="cart_id", nullable=false)
+    private Cart cart;
+
+    @ManyToOne
+    @JoinColumn(name="product_id", nullable=false)
+    private Product product;
 }
