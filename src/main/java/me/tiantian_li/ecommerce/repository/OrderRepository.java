811a92e88d16492e6c93bc96444cec28d10caf92
Tiantian Li <tiantiancscs@gmail.com>
2024-12-20 11:13:41 +1100

    Create OrderRepository.java

---
+++ src/main/java/me/tiantian_li/ecommerce/repository/OrderRepository.java
@@ -0,0 +1,10 @@
+package me.tiantian_li.ecommerce.repository;
+
+import me.tiantian_li.ecommerce.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByUserId(Long userId);
+}
