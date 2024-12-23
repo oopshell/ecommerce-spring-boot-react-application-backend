6d2c2eaa55b07a8d18eaeb6647d56b46ab931666
Tiantian Li <tiantiancscs@gmail.com>
2024-12-23 15:16:49 +1100

    Update AuthController.java

--- src/main/java/me/tiantian_li/ecommerce/controller/AuthController.java
+++ src/main/java/me/tiantian_li/ecommerce/controller/AuthController.java
@@ -1,4 +1,33 @@
 package me.tiantian_li.ecommerce.controller;
 
+import lombok.RequiredArgsConstructor;
+import me.tiantian_li.ecommerce.dto.LoginRequest;
+import me.tiantian_li.ecommerce.service.JwtService;
+import me.tiantian_li.ecommerce.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class AuthController {
+    private final AuthenticationManager authenticationManager;
+    private final UserService userService;
+    private final JwtService jwtService;
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword())
+        );
+        final UserDetails userDetails = userService.getUserByEmail(loginRequest.getEmail());
+        final String jwt = jwtService.generateToken(userDetails);
+        return ResponseEntity.ok(jwt);
+    }
 }
