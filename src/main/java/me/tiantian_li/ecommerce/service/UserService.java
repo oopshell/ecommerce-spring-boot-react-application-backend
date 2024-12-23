5036d5bb59658cb5278e20bcf7812db4c223f4e1
Tiantian Li <tiantiancscs@gmail.com>
2024-12-23 14:27:20 +1100

    Create UserService.java

---
+++ src/main/java/me/tiantian_li/ecommerce/service/UserService.java
@@ -0,0 +1,41 @@
+package me.tiantian_li.ecommerce.service;
+
+import lombok.RequiredArgsConstructor;
+import me.tiantian_li.ecommerce.dto.ChangePasswordRequest;
+import me.tiantian_li.ecommerce.exception.ResourceNotFoundException;
+import me.tiantian_li.ecommerce.model.User;
+import me.tiantian_li.ecommerce.repository.UserRepository;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    private User registerUser(User user) {
+        if (userRepository.findByEmail(user.getEmail()).isPresent()) {
+            throw new IllegalStateException("Email already exists");
+            // use IllegalStateException instead of RuntimeException, because it is more specific,
+            // indicating an invalid application state for this operation
+        }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRole(User.Role.USER);
+        return userRepository.save(user);
+    }
+
+    public User getUserByEmail(String email) {
+        return userRepository.findByEmail(email).orElseThrow(() -> new ResourceNotFoundException("User not found"));
+    }
+
+    public void changePassword(String email, ChangePasswordRequest request) {
+        User user = getUserByEmail(email);
+        if (!passwordEncoder.matches(request.getOldPassword(), user.getPassword())) {
+            throw new BadCredentialsException("Old password is incorrect");
+        }
+        user.setPassword(passwordEncoder.encode(request.getNewPassword()));
+        userRepository.save(user);
+    }
+}
