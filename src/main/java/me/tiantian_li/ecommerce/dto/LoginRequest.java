41ef522466c9c951eff1b710c2fe4568378a176c
Tiantian Li <tiantiancscs@gmail.com>
2024-12-23 15:05:40 +1100

    Create LoginRequest.java

---
+++ src/main/java/me/tiantian_li/ecommerce/dto/LoginRequest.java
@@ -0,0 +1,13 @@
+package me.tiantian_li.ecommerce.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequest {
+    private String email;
+    private String password;
+}
