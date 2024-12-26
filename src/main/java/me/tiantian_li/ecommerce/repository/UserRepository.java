432485f056297e9e4e9a41f4cf491427d0182893
Tiantian Li <tiantiancscs@gmail.com>
2024-12-26 11:27:58 +1100

    Update UserRepository.java

--- src/main/java/me/tiantian_li/ecommerce/repository/UserRepository.java
+++ src/main/java/me/tiantian_li/ecommerce/repository/UserRepository.java
@@ -2,9 +2,11 @@ package me.tiantian_li.ecommerce.repository;
 
 import me.tiantian_li.ecommerce.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 }
