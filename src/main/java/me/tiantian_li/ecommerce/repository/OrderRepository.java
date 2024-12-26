730bb921c63def7f0ad6d3f393987d7ed467090c
Tiantian Li <tiantiancscs@gmail.com>
2024-12-26 11:27:55 +1100

    Update OrderRepository.java

--- src/main/java/me/tiantian_li/ecommerce/repository/OrderRepository.java
+++ src/main/java/me/tiantian_li/ecommerce/repository/OrderRepository.java
@@ -2,9 +2,11 @@ package me.tiantian_li.ecommerce.repository;
 
 import me.tiantian_li.ecommerce.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findByUserId(Long userId);
 }
