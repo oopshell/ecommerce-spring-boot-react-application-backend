7994dd0b5068da59289fae96de0799ac64c2fca6
Tiantian Li <tiantiancscs@gmail.com>
2024-12-26 11:27:46 +1100

    Update JwtAuthenticationFilter.java

--- src/main/java/me/tiantian_li/ecommerce/config/JwtAuthenticationFilter.java
+++ src/main/java/me/tiantian_li/ecommerce/config/JwtAuthenticationFilter.java
@@ -11,14 +11,18 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @RequiredArgsConstructor
+@Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
+
+    
 
     @Override
     public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
