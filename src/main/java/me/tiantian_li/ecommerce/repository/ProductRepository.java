042ba2f9304ec7aad57dc32db9a49ad55efa6259
Tiantian Li <tiantiancscs@gmail.com>
2024-12-26 11:27:56 +1100

    Update ProductRepository.java

--- src/main/java/me/tiantian_li/ecommerce/repository/ProductRepository.java
+++ src/main/java/me/tiantian_li/ecommerce/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package me.tiantian_li.ecommerce.repository;
 
 import me.tiantian_li.ecommerce.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
