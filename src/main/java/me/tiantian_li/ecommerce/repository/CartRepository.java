2649b7a46a4b6169d9f122bc63014b7467e8cf46
Tiantian Li <tiantiancscs@gmail.com>
2024-12-26 11:27:51 +1100

    Update CartRepository.java

--- src/main/java/me/tiantian_li/ecommerce/repository/CartRepository.java
+++ src/main/java/me/tiantian_li/ecommerce/repository/CartRepository.java
@@ -2,9 +2,11 @@ package me.tiantian_li.ecommerce.repository;
 
 import me.tiantian_li.ecommerce.model.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
     Optional<Cart> findByUserId(Long userId);
 }
