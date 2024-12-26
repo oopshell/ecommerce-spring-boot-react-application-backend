c2f140001996bc14f67eb71f9141cc92c20019c4
Tiantian Li <tiantiancscs@gmail.com>
2024-12-26 11:27:49 +1100

    Update EcommerceApplication.java

--- src/main/java/me/tiantian_li/ecommerce/EcommerceApplication.java
+++ src/main/java/me/tiantian_li/ecommerce/EcommerceApplication.java
@@ -3,11 +3,17 @@ package me.tiantian_li.ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication//(scanBasePackages = {
+//		"me.tiantian_li.ecommerce.model",
+//		"me.tiantian_li.ecommerce.repository",
+//		"me.tiantian_li.ecommerce.service",
+//		"me.tiantian_li.ecommerce.config"
+//})
 @EntityScan(basePackages = "me.tiantian_li.ecommerce.model")
-@EnableJpaRepositories(basePackages = "me.tiantian_li.ecommerce.repository")
+//@EnableJpaRepositories(basePackages = "me.tiantian_li.ecommerce.repository")
 public class EcommerceApplication {
 
 	public static void main(String[] args) {
