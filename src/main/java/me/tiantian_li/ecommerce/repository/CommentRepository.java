3abcf271f8780875426905ba411cc88aef5ede6a
Tiantian Li <tiantiancscs@gmail.com>
2024-12-26 11:27:53 +1100

    Update CommentRepository.java

--- src/main/java/me/tiantian_li/ecommerce/repository/CommentRepository.java
+++ src/main/java/me/tiantian_li/ecommerce/repository/CommentRepository.java
@@ -2,9 +2,11 @@ package me.tiantian_li.ecommerce.repository;
 
 import me.tiantian_li.ecommerce.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findByProductId(Long productId);
 }
