b15fe4b862548eec4cb60dd5189c6fbbb138d5f3
Tiantian Li <tiantiancscs@gmail.com>
2024-12-28 15:35:18 +1100

    Update JwtAuthenticationFilter.java

--- src/main/java/me/tiantian_li/ecommerce/config/JwtAuthenticationFilter.java
+++ src/main/java/me/tiantian_li/ecommerce/config/JwtAuthenticationFilter.java
@@ -4,7 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
+//import lombok.RequiredArgsConstructor;
 import me.tiantian_li.ecommerce.service.JwtService;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -16,13 +16,20 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
 
-    
+//    public JwtAuthenticationFilter() {
+//        this.jwtService = null;
+//        this.userDetailsService = null;
+//    }
+    public JwtAuthenticationFilter(JwtService jwtService, UserDetailsService userDetailsService) {
+        this.jwtService = jwtService;
+        this.userDetailsService = userDetailsService;
+    }
 
     @Override
     public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
